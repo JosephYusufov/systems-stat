8ddadc8033ed2f78b8a7a492624b5e28a5e8d4a2
JosephYusufov <jyusufov00@stuy.edu>
2019-11-13 15:33:12 -0500

    jason lam

---
+++ stat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(){
+    struct stat statbuffer;
+    
+    return 0;
+}
