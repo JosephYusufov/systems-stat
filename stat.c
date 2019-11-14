a60d8f21cb16a301c896bb2d73d4594dcb3b43c7
JosephYusufov <jyusufov00@stuy.edu>
2019-11-13 23:24:57 -0500

    added makefile, finished assgmnt

--- stat.c
+++ stat.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,7 +11,13 @@
 int main(){
     struct stat statbuffer;
     stat("./stat.c", &statbuffer);
-    printf("\tsize: %lld\n", statbuffer.st_size);
+    printf("\n********************\n");
+    printf("Joseph Yusufov Stats Assignment\n\n");
+    printf("Due: Wed Nov 14th, 2019\n");
+    printf("********************\n\n");
+    printf("\tname: \"stat.c\"\n");
+    printf("\tsize: %lldB\n", statbuffer.st_size);
     printf("\tmode: %o\n", statbuffer.st_mode);
+    printf("\taccessed: %s\n", ctime(&statbuffer.st_atime));
     return 0;
 }
